cadfdfac9abf9d9be85bcaf8aba959cc82cf23c5
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-07-21 11:56:37 -0300

    realizando exercios

--- ex02.py
+++ ex02.py
@@ -1,3 +1,8 @@
-#Nesse exercio irei responder a um usuario.
+# Nesse exercio irei responder a um usuario.
 nome = input("Qual seu nome?")
 print ("Olá", nome,"Seja bem vindo!")
+# Temos outra opção que é usando o .format
+print ("Olá {}, é um prazer te conhecer!" .format(nome))
+# Podemos ver que usando o .format fica muito mais ajustado o texto
+
+
---
+++ ex03.py
@@ -0,0 +1,6 @@
+# Somando dois numeros.
+a = int(input("Digite uma valor: "))
+b = int(input("Digite uma valor: "))
+s = (a+b)
+print("A soma entre {} e {} é igual a {}" .format(a, b, s))
+# Se fossemos somar numeros decimais temos que coloar float caso contratio nao vamos conseguir o resultado esperado.
