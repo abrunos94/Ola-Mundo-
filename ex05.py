6030d5cc6dc2c17b5b0077273734f0ea563ec25d
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-07-21 21:53:46 -0300

    Create ex05.py

---
+++ ex05.py
@@ -0,0 +1,11 @@
+# Exercicio de Aritimetica
+n1 = int(input("Digite um numero:"))
+n2 = int(input("Digite um numero:"))
+s = n1 + n2   # Adiçao
+m = n1 * n2   # Multiplicaçao
+d = n1 / n2   # Divisao
+di = n1 // n2 # Divisao inteira
+e = n1 ** n2  # Elevado
+print(" A soma é {}; \n Multiplicação é {}; \n Divisão {};".format(s, m, d))
+print(" Divisão interira {}; \n Potencia {};" .format(di, e))
+# o \n émuitoutilna quebra de linha!
