7b0cc9d6b31e882c50d7f07feb77c2e149c73e47
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-07-25 10:41:42 -0300

    exxercicio

---
+++ ex023.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre:
+nome = str(input("Digite seu nome completo: "))
+print (" Analisando seu nome...")
+print (" Seu nome em maiusculas é {}" .format(nome.upper()))
+print ("Seu nome em minusculas é {}" .format(nome.lower()))
+print ("Seu nome tem ao todo {} letras" .format(len(nome)))
