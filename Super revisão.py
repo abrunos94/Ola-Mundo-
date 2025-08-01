f3e7c4e03678ea116d04d5d478292dcc2cb696ca
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-08-01 16:04:01 -0300

    Update revisao

---
+++ Super revisão.py
@@ -0,0 +1,10 @@
+# Nesse projeto sera demosntrado e explicado as principais funçoes.
+# Sera demonstrado linhas de codigos (basta tirar a "#" de cometario para seguir)
+         # Modulo 1 Entrada e saida de variaveis;
+
+nome = input('Qual seu nome?')
+idade = input('Quantos anos voce tem?')
+#print(' Olá {}! Voce tem {} anos!' .format(nome, idade))
+#print(f'Olá {nome}! Voce tem {idade} anos!')
+
+   #pode ver que existe dois jeitos de usar or format como vimos acima!
