f9b4cfc0a99d4538bef8057bb15a2095235e5d2e
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-08-01 16:08:20 -0300

    Update Super revisão.py

--- Super revisão.py
+++ Super revisão.py
@@ -1,14 +1,19 @@
+# SUPER REVISÃO - PYTHON MUNDO 1
+
 # Neste projeto será demonstrado e explicado as principais funções.
 # Serão mostradas linhas de código (basta remover o "#" dos comentários para executar)
 
-# Módulo 1 - Entrada e saída de variáveis:
+# ---------------------------------------------------------
+# MÓDULO 1 - Entrada e saída de variáveis:
+# ---------------------------------------------------------
 
-# EXEMPLO:
 nome = input('Qual seu nome? ')
 idade = input('Quantos anos você tem? ')
 
-# EXEMPLO:
+# Duas formas de formatar strings:
 # print('Olá {}! Você tem {} anos!'.format(nome, idade))
 # print(f'Olá {nome}! Você tem {idade} anos!')
 
-# Podemos ver que existem duas formas de formatar textos, como mostrado acima.
+# EXEMPLO:
+# Entrada: nome = Alex, idade = 30
+# Saída: Olá Alex! Você tem 30 anos!
