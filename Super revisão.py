a5c97ad01273f4a2f55b13dafc3dc5d6e1fad978
Alex Bruno da silva mariano <abrunos57@gmail.com>
2025-08-04 11:28:52 -0300

    update

--- Super revisão.py
+++ Super revisão.py
@@ -7,8 +7,8 @@
 # MÓDULO 1 - Entrada e saída de variáveis:
 # ---------------------------------------------------------
 
-nome = input('Qual seu nome? ')
-idade = input('Quantos anos você tem? ')
+nome =  input('Qual seu nome? ')
+#idade = input('Quantos anos você tem? ')
 
 # Duas formas de formatar strings:
 # print('Olá {}! Você tem {} anos!'.format(nome, idade))
@@ -17,3 +17,17 @@ idade = input('Quantos anos você tem? ')
 # EXEMPLO:
 # Entrada: nome = Alex, idade = 30
 # Saída: Olá Alex! Você tem 30 anos!
+
+# ---------------------------------------------------------
+# MÓDULO 2 - Tipos Primitivos e Conversão de Tipos
+# ---------------------------------------------------------
+# Tipos primitivos, segue abaixo alguns exemplos de primitivos:
+# int: numeros inteiros ( 10, -4);
+# float: numeros com casas decimais (3.14, 9.8888....)
+# str : texto ou caracteres ("texto")
+# boll : valores boleanos ( logico), (True, False)
+# Exemplo:
+idade = int(input(' Quantos anos voce tem? ')) # int mostra que o numero é inteiro.
+altura = float(input(' Qual sua altura? ')) # float aqui pede numeres decimais
+str(print(' Seu nome é {}, voce tem {}, e sua altura é {}.' .format(nome, idade, altura)))
+str(print(f'Seu nome é {nome}, voce tem {idade} e sua altura é {altura}'))
